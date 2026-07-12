d015a1aaae2b8f34f8c3b3deaddab1c662d2045e
Azizo <asis@example.com>
2026-07-13 02:41:06 +0300

    chore: setup project structure, fix layouts, configure fonts, restore homepage, and update next.js version

---
+++ app/layout.js
@@ -0,0 +1,39 @@
+import Header from "@/app/_components/Header";
+import { ReservationProvider } from "@/app/_components/ReservationContext";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-josefin",
+});
+
+import "@/app/_styles/globals.css";
+
+export const metadata = {
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.variable} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative font-sans`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+}
